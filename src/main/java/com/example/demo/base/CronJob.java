3bff6e5fd11ce322335859b1592723dedbbc5e0c
jiajing <imjiajing1222@gmail.com>
2023-07-07 20:37:01 +0800

    1. added cronjob for update expired appointment

--- src/main/java/com/example/demo/base/CronJob.java
+++ src/main/java/com/example/demo/base/CronJob.java
@@ -10,7 +10,8 @@ import com.google.cloud.firestore.CollectionReference;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -87,11 +88,99 @@ public class CronJob {
                         .update("donationTimes", donationTimes);
 
             }
-
-
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
             // Handle any exceptions that occur during the process
+        }
+    }
+
+    @Scheduled(cron = "0 */30 * * * *")
+    public void updateExpiredAppointment() {
+        Firestore firestore = FirestoreClient.getFirestore();
+
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // Format the current date to match the 'appmntDate' format in Firestore ('MMMM d, yyyy')
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        String formattedCurrentDate = currentDate.format(formatter);
+
+        // Create a query to filter appointments where 'appmntDate' is less than the current date and 'aStatus' is 'Pending'
+        Query query = firestore.collection("appointment")
+                .whereGreaterThan("appmntDate", formattedCurrentDate)
+                .whereEqualTo("aStatus", "Pending");
+
+        // Retrieve the appointments that match the query
+        ApiFuture<QuerySnapshot> querySnapshotFuture = query.get();
+
+        try {
+            QuerySnapshot querySnapshot = querySnapshotFuture.get();
+
+            // Create a WriteBatch for batch updates
+            WriteBatch batch = firestore.batch();
+
+            for (QueryDocumentSnapshot documentSnapshot : querySnapshot.getDocuments()) {
+                DocumentReference documentRef = documentSnapshot.getReference();
+
+                // Update the 'aStatus' field to 'Expired' in the batch
+                batch.update(documentRef, "aStatus", "Expired");
+            }
+
+            // Commit the batch updates
+            batch.commit().get();
+        } catch (Exception e) {
+            // Handle any exceptions that may occur
+            e.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "0 */30 * * * *")
+    public void updateExpiredAppointmentBySameMonth() {
+        Firestore firestore = FirestoreClient.getFirestore();
+
+        // Get the current date
+        LocalDate currentDate = LocalDate.now();
+
+        // Format the current date to match the 'appmntDate' format in Firestore ('MMMM d, yyyy')
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        String formattedCurrentDate = currentDate.format(formatter);
+
+        // Create a query to filter appointments where 'appmntDate' is less than or equal to the current date
+        Query query = firestore.collection("appointment")
+                .whereLessThanOrEqualTo("appmntDate", formattedCurrentDate)
+                .whereEqualTo("aStatus", "Pending");
+
+        // Retrieve the appointments that match the query
+        ApiFuture<QuerySnapshot> querySnapshotFuture = query.get();
+
+        try {
+            QuerySnapshot querySnapshot = querySnapshotFuture.get();
+
+            // Create a WriteBatch for batch updates
+            WriteBatch batch = firestore.batch();
+
+            for (QueryDocumentSnapshot documentSnapshot : querySnapshot.getDocuments()) {
+                DocumentReference documentRef = documentSnapshot.getReference();
+
+                // Get the 'appmntDate' value from the document
+                String appmntDate = documentSnapshot.getString("appmntDate");
+
+                // Parse the 'appmntDate' string to LocalDate
+                LocalDate appmntLocalDate = LocalDate.parse(appmntDate, formatter);
+
+                // Compare the month and day of 'appmntDate' with the current date
+                if (appmntLocalDate.getMonthValue() == currentDate.getMonthValue()
+                        && appmntLocalDate.getDayOfMonth() < currentDate.getDayOfMonth()) {
+                    // Update the 'aStatus' field to 'Expired' in the batch
+                    batch.update(documentRef, "aStatus", "Expired");
+                }
+            }
+
+            // Commit the batch updates
+            batch.commit().get();
+        } catch (Exception e) {
+            // Handle any exceptions that may occur
+            e.printStackTrace();
         }
     }
 }
